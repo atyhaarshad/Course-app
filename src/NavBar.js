834a42e60730fd18a77869e62078fb2ad09bfe71
atyha <atyhaarshad1@hotmail.com>
2019-06-19 10:40:06 +0100

    components, containers added

---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import {Link} from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <ul>
+      <li> Courses </li>
+      <li> Bookings </li>
+      <li> Customers </li>
+    </ul>
+  )
+}
+
+export default NavBar;
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NavBar from '../NavBar.js';
+
+const MainContainer = () => {
+
+  return (
+    <div>
+    <NavBar />
+    <h1>This is the Main container!</h1>
+    </div>
+  );
+
+}
+
+export default MainContainer;
